699920dc2f45f11a1469dbf6458a43f03b2710f2
jiachenli011 <jli114@syr.edu>
2024-10-04 15:23:26 -0400

    finish unibrow v0.2

--- code/unibrow.py
+++ code/unibrow.py
@@ -1,6 +1,13 @@
 '''
 Solution unibrow.py
 '''
+
+# Upload a file using Streamlit, accepting files of types .csv, .xlsx, and .json. 
+# Get the file extension of the uploaded file to determine its type and load the file accordingly. 
+#Retrieve all column names from the loaded dataframe and allow the user to select which columns to display using a multi-select widget, with all columns selected by default. 
+# Provide an option to toggle data filtering, which enables the user to filter the dataframe by selecting a specific value from text-based columns. 
+# Display the filtered or unfiltered dataframe using Streamlit, along with a statistical summary of the numerical columns.
+
 import pandas as pd
 import streamlit as st
 import pandaslib as pl
@@ -12,6 +19,19 @@ file = st.file_uploader("Upload a file", type=['csv', 'json', 'xlsx'])
 if file:
     file_type = pl.get_file_extension(file.name)
     file_df = pl.load_file(file, file_type)
-    
+    cols = pl.get_column_names(file_df)
+    selected_cols = st.multiselect("Select columns to display", cols, default=cols)
+    if st.toggle("Filter data"):
+        stcols = st.columns(3)
+        text_cols = pl.get_columns_of_type(file_df, 'object')
+        filter_col = stcols[0].selectbox("Select column to filter", text_cols)
+        if filter_col:
+            vals = pl.get_unique_values(file_df, filter_col)
+            val = stcols[1].selectbox("Select value to filter On", vals)
+            file_df_show = file_df[file_df[filter_col] == val][selected_cols]
+    else:
+        file_df_show = file_df[selected_cols]
+    st.dataframe(file_df_show)
+    st.dataframe(file_df_show.describe())
 
 
